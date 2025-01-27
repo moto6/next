ef8886f9ddd0bb9f712998eb5abc7f6af15e3e60
dong <ultraraptor@naver.com>
2025-01-27 21:08:56 +0900

    good

--- nextjs14/app/about-us/page.tsx
+++ nextjs14/app/about-us/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Metadata} from "next";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from "next/image";
 
 export const metadata: Metadata = {
@@ -7,16 +8,91 @@ export const metadata: Metadata = {
 }
 
 export default function AboutUsPage() {
+    const images = [
+        "/img/desc1.webp",
+        "/img/desc2.webp",
+        "/img/desc3.webp",
+    ];
+
+    const imgSize = {width: 800, height: 600};
     return (
-        <div className="m-1">
-            <h1>About Us</h1>
-            <Image
-                src="/img/DALLE_2025-01-27_dong.webp"
-                width={800}
-                height={800}
-                alt="About Us logo img"
-                unoptimized
-            />
+        <div className="container">
+            <div
+                id="carouselIndicators"
+                className="carousel slide"
+                data-bs-ride="carousel"
+            >
+
+                <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
+                            className="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                </div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <Image src="/img/desc1.webp"
+                               width={imgSize.width}
+                               height={imgSize.height}
+                               className="d-block w-100"
+                               alt="..."
+                               unoptimized
+                        />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <Image src="/img/desc2.webp"
+                               width={imgSize.width}
+                               height={imgSize.height}
+                               className="d-block w-100"
+                               alt="..."
+                               unoptimized
+                        />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <Image src="/img/desc3.webp"
+                               width={imgSize.width}
+                               height={imgSize.height}
+                               className="d-block w-100"
+                               alt="..."
+                               unoptimized
+                        />
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Carousel Controls */}
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExample"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExample"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
-    )
+    );
 }
