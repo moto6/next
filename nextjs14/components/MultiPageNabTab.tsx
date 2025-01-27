8d551dff5cf71c214688e2d15933beb205a2fe8c
dong <ultraraptor@naver.com>
2025-01-27 09:36:50 +0900

    good

--- nextjs14/components/MultiPageNabTab.tsx
+++ nextjs14/components/MultiPageNabTab.tsx
@@ -12,13 +12,12 @@ const MultiPageNabTab: React.FC<{ children: React.ReactNode }> = ({children}) =>
 
         <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div className="flex bg-gray-100 p-2 border-b">
-
                 {tabs.map((tab) => (
                     <div
                         key={tab.id}
                         className={`flex items-center px-4 py-2 ${pathname === tab.href ? "bg-white border" : "bg-gray-200"}`}
                     >
-                        <Link href={tab.href} className="mr-2">
+                        <Link href={tab.href} className="nav-link mr-2">
                             {tab.label}
                         </Link>
                         <button onClick={() => removeTab(tab.id)} className="text-red-500">
