fc4dd665c29a2c224497341fad8c6ca3bc20665f
dong <ultraraptor@naver.com>
2025-01-28 13:18:01 +0900

    good

--- nextjs14/components/aspect/FormRadioButton.tsx
+++ nextjs14/components/aspect/FormRadioButton.tsx
@@ -1,28 +1,39 @@
 'use client'
 
-import React from "react";
+import React, {useState} from "react";
 
 export default function FormRadioButton() {
+    const [selectedValue, setSelectedValue] = useState("Base64");
+
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
     return (
         <div>
             <div className="form-check form-check-inline">
-                <input type="radio"
-                       id="outputBase64"
-                       name="outputFormat"
-                       className="form-check-input"
-                       value="Base64"
+                <input
+                    type="radio"
+                    id="outputBase64"
+                    name="outputFormat"
+                    className="form-check-input"
+                    value="Base64"
+                    checked={selectedValue === "Base64"}
+                    onChange={handleChange}
                 />
-                <label htmlFor="outputBase64"
-                       className="form-check-label">Base64</label>
+                <label htmlFor="outputBase64" className="form-check-label">Base64</label>
             </div>
             <div className="form-check form-check-inline">
-                <input type="radio"
-                       id="outputHex"
-                       name="outputFormat"
-                       className="form-check-input"
-                       value="Hex"/>
-                <label
-                    htmlFor="outputHex" className="form-check-label">Hex</label>
+                <input
+                    type="radio"
+                    id="outputHex"
+                    name="outputFormat"
+                    className="form-check-input"
+                    value="Hex"
+                    checked={selectedValue === "Hex"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="outputHex" className="form-check-label">Hex</label>
             </div>
         </div>
     );
