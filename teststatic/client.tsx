592aacde7fc3826a3ded13bf661dd0913bd2a345
dong <ultraraptor@naver.com>
2025-01-28 13:53:15 +0900

    good

---
+++ teststatic/client.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+export default function client() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+
+// import { useSearchParams } from 'next/navigation'
+//
+// export default function SearchBar() {
+//     const searchParams = useSearchParams()
+//
+//     const search = searchParams.get('search')
+//
+//     // URL -> `/dashboard?search=my-project`
+//     // `search` -> 'my-project'
+//     return <>Search: {search}</>
+// }
+//https://nextjs.org/docs/app/api-reference/functions/use-search-params
---
+++ teststatic/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+//https://nextjs.org/docs/app/api-reference/file-conventions/page
