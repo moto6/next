eacf34b58cf6d1b199cee7449dd7a010a52ba113
dong <ultraraptor@naver.com>
2025-01-30 12:02:12 +0900

    add bootstrap

---
+++ nextjs14/components/aggregate/RootComponent.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+// import "../../public/bootstrap/css/bootstrap.css";
+// import "../../public/bootstrap/js/bootstrap.js";
+
+export default function RootComponent() {
+    return (
+        <div>
+        </div>
+    );
+}
