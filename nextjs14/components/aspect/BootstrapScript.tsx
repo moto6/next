35ef7bc825570ea4bd9b4413bdc0d86dd205debd
dong <ultraraptor@naver.com>
2025-01-31 18:42:56 +0900

    config setting

--- nextjs14/components/aspect/BootstrapScript.tsx
+++ nextjs14/components/aspect/BootstrapScript.tsx
@@ -3,8 +3,10 @@ import { loadEnvConfig } from '@next/env'
 export default function BootstrapScript() {
     const projectDir = process.cwd()
     loadEnvConfig(projectDir)
-    console.log(projectDir);
-    console.log(`ENV : ${process.env.ENVIRONMENT}`);
+    // console.log(projectDir);
+    // console.log(`ENV : ${process.env.ENVIRONMENT}`);
+    // console.log('Base Path:', process.env.NEXT_PUBLIC_BASE_PATH);
+    // console.log('Asset Prefix:', process.env.NEXT_PUBLIC_ASSET_PREFIX);
     return (
         <script src={`${process.env.NEXT_PUBLIC_ASSET_PREFIX}/bootstrap/js/bootstrap.bundle.min.js`} defer/>
     );
--- nextjs14/next.config.js
+++ nextjs14/next.config.js
@@ -1,8 +1,10 @@
 const nextConfig = {
     output: 'export',
     productionBrowserSourceMaps: false,
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
+    // basePath: process.env.NEXT_PUBLIC_BASE_PATH || "ERROR_BASE_PATH/",
+    // assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "ERROR_ASSET_PREFIX/",
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
 }
 
 module.exports = nextConfig
