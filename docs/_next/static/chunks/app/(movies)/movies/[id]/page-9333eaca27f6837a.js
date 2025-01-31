c9eb2910220ea3f1ded75e365d42349a78d79da4
dong <ultraraptor@naver.com>
2025-01-31 19:12:17 +0900

    deploy

--- docs/_next/static/chunks/app/(movies)/movies/[id]/page-9333eaca27f6837a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[875,608,614,160,938,409,34,550,631,702,663,74,928,653,431],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
